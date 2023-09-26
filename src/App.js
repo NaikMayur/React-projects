950dd421e59235bd7f476adcd90c1bb3c208d9c4
Mayur Naik <mayurnaik364@gmail.com>
2023-09-26 12:45:04 +0530

    Made an static react application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
 function App() {
-  const name = "Mayur";
-  const x = true;
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Docters appointment",
+      day: "Feb 5th",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "School meeting",
+      day: "sept 20th",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "food shopping",
+      day: "oct 23rd",
+      reminder: false,
+    },
+  ]);
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
     <div className="container">
-      <Header />
-      <Tasks />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,15 @@
 import React from "react";
-const tasks = [
-  {
-    id: 1,
-    text: "Docters appointment",
-    day: "Feb 5th",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "School meeting",
-    day: "sept 20th",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "food shopping",
-    day: "oct 23rd",
-    reminder: false,
-  },
-];
-const Tasks = () => {
+import Task from "./Task";
+const Tasks = (props) => {
   return (
     <>
-      {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+      {props.tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={props.onDelete}
+          onToggle={props.onToggle}
+        />
       ))}
     </>
   );
