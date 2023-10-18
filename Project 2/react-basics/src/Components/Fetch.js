ab48b1db2b07eab51802dedbae62ca522a692503
Mayur Naik <mayurnaik364@gmail.com>
2023-10-18 15:15:51 +0530

    fetch

--- Project 2/react-basics/src/Components/Fetch.js
+++ Project 2/react-basics/src/Components/Fetch.js
@@ -1,20 +1,38 @@
 import React, { useEffect, useState } from "react";
 function Fetch() {
-  const [data, setData] = useState("");
   const url = "https://api.github.com/users/NaikMayur";
+  const [data, setData] = useState("");
+  const [error, setError] = useState(null);
   useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        return res.json();
-      })
+    const fetchApi = new Promise((resolve, reject) => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Check the url properly!!");
+          }
+          return res.json();
+        })
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          setError(err.message);
+          reject(err);
+        });
+    });
+    fetchApi
       .then((res) => {
         setData(res);
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }, []);
   return (
     <div>
+      {error && <p>Error: {error}</p>}
       <ul>
-        <li>{data.login && <li>{data}</li>}</li>
+        <li>{data.login}</li>
       </ul>
     </div>
   );
