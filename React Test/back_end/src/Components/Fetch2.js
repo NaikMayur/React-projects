d715db04ddec183a854233b575d668af4d9dcce8
Mayur Naik <mayurnaik364@gmail.com>
2023-10-27 17:42:02 +0530

    fetch

--- React Test/back_end/src/Components/Fetch2.js
+++ React Test/back_end/src/Components/Fetch2.js
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from "react";
 function Fetch2() {
   const [data, setData] = useState("");
   const [error, setErr] = useState(null);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.ypicode.com/users")
+    fetch("https://sonplaceholder.typicode.com/users")
       .then((res) => {
         if (!res.ok) {
-          throw new Error("Network Error: could not fetch");
+          throw new Error(res.status);
         }
         return res.json();
       })
@@ -15,25 +16,29 @@ function Fetch2() {
         setData(res);
       })
       .catch((err) => {
-        setErr(err);
+        setErr(err.message);
       });
   }, []);
+
   console.log(data);
+
   return (
     <>
-      {error ? (
-        <p>{error}</p>
-      ) : data ? (
-        <ul>
-          {data.map((posts) => (
-            <li key={posts.id}>
-              <h3>{posts.name}</h3>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <ul>
+        {error ? (
+          <p>Error: {error}</p>
+        ) : data ? (
+          <div>
+            {data.map((posts) => (
+              <li key={posts.id}>
+                <h3>{posts.name}</h3>
+              </li>
+            ))}
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </ul>
     </>
   );
 }
