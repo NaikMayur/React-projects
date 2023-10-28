543f00a19ec911fc93e4f141d514ba1c37cb6d47
Mayur Naik <mayurnaik364@gmail.com>
2023-10-28 12:03:24 +0530

    fetch

--- React Test/back_end/src/Components/Fetch2.js
+++ React Test/back_end/src/Components/Fetch2.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
 function Fetch2() {
   const [data, setData] = useState("");
   const [error, setErr] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    const obj = {
+      id: 1,
+      title: "Sample Title",
+      body: "Sample body content",
+    };
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    })
       .then((res) => {
         if (!res.ok) {
           throw new Error(res.status);
@@ -14,13 +25,17 @@ function Fetch2() {
       })
       .then((res) => {
         setData(res);
+        console.log("data:", obj);
+        console.log("data sent :", res);
       })
       .catch((err) => {
         setErr(`${err.message}`);
       });
-  }, []);
 
-  console.log(data);
+    // axios.post("https://jsonplaceholder.typicode.com/users", obj, {
+    //   headers: { "Content-Type": "application/json" },
+    // });
+  }, []);
 
   return (
     <>
@@ -29,13 +44,15 @@ function Fetch2() {
           <p>Error: {error}</p>
         ) : data ? (
           <div>
-            {data.map((posts) => (
+            <p>Data sent: {data.title}</p>
+            <br />
+            {/* {data.map((posts) => (
               <li key={posts.id}>
                 <i>
                   <h4>{posts.name}</h4>
                 </i>
               </li>
-            ))}
+            ))} */}
           </div>
         ) : (
           <p>Loading...</p>
